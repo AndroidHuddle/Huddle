fb3157aa99eedbbd7f20a38a2067cbfe5ddf0ae7
Victor Hom <victorhom@Victors-MBP-2.home>
2016-11-13 18:46:38 -0500

    add initial play service work

--- app/src/main/java/com/example/badhri/huddle/MainActivity.java
+++ app/src/main/java/com/example/badhri/huddle/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.badhri.huddle;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.badhri.huddle.activities.EventDetailActivity;
 import com.example.badhri.huddle.models.Business;
@@ -18,7 +23,12 @@ import java.util.ArrayList;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cz.msebera.android.httpclient.Header;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
 
+// need to add annotations to the activity that uses the runtime permissions
+// requesting for location requires runtime permissions
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -28,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         YelpClient c = MainClientApp.getYelpRestClient();
         c.search("restaurants", "San Francisco", searchHandler());
+
+        // not sure if this works correctly, i didn't get asked for permission
+        // but it says it's true; i'm not sure how to turn off permissions for location
+        if (checkPermission(this)){
+            System.out.println("I have have permissions?");
+        }
+        MainActivityPermissionsDispatcher.checkLocationsPermissionWithCheck(this);
 
     }
 
@@ -57,5 +74,29 @@ public class MainActivity extends AppCompatActivity {
     public void openEventDetails() {
         Intent eventIntent = new Intent(this, EventDetailActivity.class);
         this.startActivity(eventIntent);
+    }
+
+    // SCRATCH Work to figure out runtime permissions
+    final String permission = Manifest.permission.ACCESS_COARSE_LOCATION;
+
+//    @OnClick(R.id.btnLocationPermissions)
+    @NeedsPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+    public void checkLocationsPermission() {
+        // trigger check for location
+
+        Toast.makeText(this,"checkLocationsPermission", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+//        Toast.makeText(this,"onRequestPermissionsResult", Toast.LENGTH_LONG);
+    }
+
+    public static boolean checkPermission(final Context context) {
+        return ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
 }
