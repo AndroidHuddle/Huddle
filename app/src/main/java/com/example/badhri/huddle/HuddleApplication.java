e1f7e73282ad3da7ed07b8b16bd358827763111d
ALicia P <pericia@gmail.com>
2016-11-14 22:52:55 -0500

    Integrate the login process into the app

--- app/src/main/java/com/example/badhri/huddle/HuddleApplication.java
+++ app/src/main/java/com/example/badhri/huddle/HuddleApplication.java
@@ -1,7 +1,10 @@
 package com.example.badhri.huddle;
 
 import android.app.Application;
+import android.content.Intent;
+import android.widget.Toast;
 
+import com.example.badhri.huddle.activities.MainActivity;
 import com.example.badhri.huddle.parseModels.Attendees;
 import com.example.badhri.huddle.parseModels.Events;
 import com.example.badhri.huddle.parseModels.User;
@@ -14,6 +17,8 @@ import com.parse.interceptors.ParseLogInterceptor;
  */
 
 public class HuddleApplication extends Application {
+    private AuthCallback authCallback;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -27,5 +32,38 @@ public class HuddleApplication extends Application {
                 .applicationId("androidhuddle") // should correspond to APP_ID env variable
                 .addNetworkInterceptor(new ParseLogInterceptor())
                 .server("http://androidhuddle.herokuapp.com/parse").build());
+        setupCallbackforLogin();
+    }
+
+    private void setupCallbackforLogin() {
+        final TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.CONSUMER_KEY,
+                BuildConfig.CONSUMER_SECRET);
+
+        Fabric.with(this, new TwitterCore(authConfig), new Digits.Builder().build());
+
+
+        authCallback = new AuthCallback() {
+            @Override
+            public void success(DigitsSession session, String phoneNumber) {
+                // Do something with the session
+                Toast.makeText(getBaseContext(), "login successful", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);//I don't really get why this is needed
+                startActivity(intent);
+            }
+
+            @Override
+            public void failure(DigitsException exception) {
+                // Do something on failure
+                Toast.makeText(getBaseContext(), "login NOT successful", Toast.LENGTH_LONG).show();
+                //Log.d("Digits", "Sign in with Digits failure", exception);
+            }
+        };
+    }
+
+
+    public AuthCallback getAuthCallback(){
+        return authCallback;
     }
 }
