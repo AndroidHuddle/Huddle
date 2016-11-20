2a214ef0733de49ab122bd36cb79335a0b5e3ba1
ALicia P <pericia@gmail.com>
2016-11-20 15:34:57 -0500

    Set main structure for creating an event

---
+++ app/src/main/java/com/example/badhri/huddle/activities/SelectPlaceActivity.java
@@ -0,0 +1,15 @@
+package com.example.badhri.huddle.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.badhri.huddle.R;
+
+public class SelectPlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_place);
+    }
+}
