4e52cb9b4601285d34023785c5ae74eb586437c6
Victor Hom <vh62@cornell.edu>
2016-12-04 23:12:50 -0500

    add radio button to attend, no, maybe

--- app/src/main/java/com/example/badhri/huddle/fragments/EventDetailFragment.java
+++ app/src/main/java/com/example/badhri/huddle/fragments/EventDetailFragment.java
@@ -1,31 +1,50 @@
 package com.example.badhri.huddle.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.badhri.huddle.R;
+import com.example.badhri.huddle.activities.DashboardActivity;
 import com.example.badhri.huddle.adapters.UsersAdapter;
 import com.example.badhri.huddle.models.EventNonParse;
 import com.example.badhri.huddle.models.UserNonParse;
+import com.example.badhri.huddle.parseModels.Attendees;
 import com.example.badhri.huddle.parseModels.User;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EventDetailFragment extends Fragment {
+    // matching with the exact text in strings.xml
+    public String ATTEND = "Attend";
+    public String MAYBE = "Maybe";
+    public String NOPE = "Nope";
+
+    private String filter;
+
     @BindView(R.id.rvAttendees)
     RecyclerView rvAttendees;
 
@@ -43,10 +62,26 @@ public class EventDetailFragment extends Fragment {
 
     private UserNonParse user;
 
+    @BindView(R.id.rbAttend)
+    RadioButton rbAtten;
+
+    @BindView(R.id.rbMaybe)
+    RadioButton rbMaybe;
+
+    @BindView(R.id.rbNoAttend)
+    RadioButton rbNoAttend;
+
+    @BindView(R.id.rgRadioAttendOption)
+    RadioGroup rbRadioAttendOption;
+
+    @BindView(R.id.btnSave)
+    Button btnSave;
+
 
     private ArrayList<User> users;
     private UsersAdapter usersAdapter;
     EventNonParse event;
+
 
     private Unbinder unbinder;
 
@@ -80,6 +115,7 @@ public class EventDetailFragment extends Fragment {
         Bundle bundle = this.getArguments();
         if (bundle != null) {
             event = getArguments().getParcelable("event");
+            user = getArguments().getParcelable("user");
         }
         users = new ArrayList<>();
         usersAdapter = new UsersAdapter(getActivity(), users);
@@ -96,8 +132,82 @@ public class EventDetailFragment extends Fragment {
 //        }
 
         tvEventName.setText(event.getEventName());
-        tvEventTime.setText(event.getEndTime().toString());
+        if (event.getEndTime() != null) {
+            tvEventTime.setText(event.getEndTime().toString());
+        }
+
         tvEventAddress.setText(event.getVenue());
     }
+
+    @OnClick(R.id.btnSave)
+    public void radioOptionNotify() {
+        // get selected radio button from radioGroup
+        int selectedId = rbRadioAttendOption.getCheckedRadioButtonId();
+
+        RadioButton btn = (RadioButton) getView().findViewById(selectedId);
+        Log.d("DEBUG", btn.getText().toString());
+        String decision = btn.getText().toString();
+
+        // need to update the Attendees table
+        // need the event id
+        // the new status (from the radio btn selection)
+        // the user id
+        markStatus(decision);
+    }
+
+    private void markStatus(final String decision) {
+        ParseQuery query = new ParseQuery("Attendees");
+        query.whereEqualTo("event", event.getEventId());
+        query.whereEqualTo("user", user.getParseId());
+        query.findInBackground(new FindCallback() {
+
+            @Override
+            public void done(Object o, Throwable throwable) {
+                if (o != null) {
+                    ArrayList<Attendees> al = (ArrayList<Attendees>) o;
+                    if (al.size() > 0) {
+                        Attendees a = al.get(0);
+                        if (decision.equals(ATTEND)) {
+                           a.setStatus("Attending");
+                        } else if (decision.equals(MAYBE)) {
+                            a.setStatus("Not Responded");
+                        } else {
+                            a.setStatus("Not Attending");
+                        }
+                        try {
+                            a.save();
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+
+                        Intent i = new Intent(getActivity(), DashboardActivity.class);
+                        // pass the user in the oncreate methods;
+                        i.putExtra("user", user);
+                        // in case I want to mark some sort of feedback back in the Dashboard
+                        // maybe something like coloring the tab
+                        //
+                        i.putExtra("eventId", a.getEvent());
+                        i.putExtra("status", decision);
+                        startActivity(i);
+                    }
+                }
+
+            }
+
+            @Override
+            public void done(List objects, ParseException e) {
+
+            }
+        });
+    }
+
+    private void markMaybe() {
+
+    }
+
+    private void markNotAttending() {
+
+    }
+
 
 }
